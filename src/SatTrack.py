262e8ecadf621d9154e76eb5d3e9bff46297200f
methurston <matt.thurston@gmail.com>
2016-04-14 12:33:36 -0600

    Fix params for Like clause

--- src/SatTrack.py
+++ src/SatTrack.py
@@ -24,7 +24,7 @@ db_name = config['datasource']['filename']
 
 def fetch_sat_tle(sat_name):
     query = 'SELECT name, lineone, linetwo FROM satellites where name like ?;'
-    params = (sat_name,)
+    params = ('%{}%'.format(sat_name),)
     # print(query)
     conn = sqlite3.connect(db_name)
     cursor = conn.cursor()
