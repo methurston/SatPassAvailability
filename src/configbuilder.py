4eca29d292f8484c6b693816efe8c7082df71039
thurston <matt.thurston@gmail.com>
2016-04-18 17:16:37 -0600

    Start building config object

--- src/configbuilder.py
+++ src/configbuilder.py
@@ -19,6 +19,34 @@ def find_db():
     return matches
 
 
+def get_db_name():
+    """Generates the datasource section of the config file.
+    sqlite3 is the only type supported"""
+    available_dbs = find_db()
+    total_dbs = len(available_dbs)
+    if total_dbs == 0:
+        db_name = raw_input('Enter name for database: ')
+    else:
+        print('Multiple Databases found')
+        for idx, db_name in enumerate(available_dbs):
+            print('{} - {}'.format(idx, db_name))
+
+        try:
+            db_index = int(raw_input('Which database do you want to use? '))
+            try:
+                db_name = available_dbs[db_index]
+            except IndexError:
+                print('That is not a valid DB')
+                get_db_name()
+        except ValueError:
+            print('Invalid entry, enter an integer.')
+            get_db_name()
+    return {'type': 'sqlite3',
+            'filename': db_name,
+            'Username': None,
+            'Password': None}
+
+
 def get_callinfo():
     callsign = raw_input('Enter Callsign: ')
     userzone = raw_input('Enter Timezone: ')
@@ -27,15 +55,45 @@ def get_callinfo():
     except pytz.UnknownTimeZoneError as err:
         print('Invalid Timezone entered.')
         userzone = raw_input('Enter Timezone: ')
-    return {'callsign': callsign,
+    return {'callsign': callsign.upper(),
             'timezone': userzone
             }
 
 
+datasource= get_db_name()
+default_location= get_callinfo()
+usersource = {
+    "host": "https://callook.info",
+    "username": None
+}
+satsource = {
+    "amsat": {
+        "host": "www.amsat.org",
+        "path": "/amsat/ftp/keps/current/",
+        "filename": "nasabare.txt"
+    },
+    "celestrak": {
+        "host": "www.celestrak.com",
+        "path": None,
+        "filenames": {
+            "ham": "amateur.txt",
+            "amateur": "amateur.txt",
+            "weather": "weather.txt",
+            "wx": "weather.txt",
+            "noaa": "noaa.txt"}
+    }
+}
 
-test = find_db()
-blah = get_callinfo()
-for i in test:
-    print(i)
+def get_age_threshold():
+    pass
 
-print(blah)
+blah = {}
+blah['datasource'] = datasource
+blah['satsource'] = satsource
+blah['default_location'] = default_location
+blah['usersource'] = usersource
+
+
+
+print(json.dumps(blah, indent=2))
+
