4d006028670754cfdecb8676bd85960b3e0f50f7
thurston <matt.thurston@gmail.com>
2016-04-27 23:05:31 -0600

    Add method to input street address if callsign is not found

--- src/UserManager.py
+++ src/UserManager.py
@@ -20,19 +20,11 @@ callsign_url = config['usersource']['host']
 db_name = config['datasource']['filename']
 
 
+
 def lookup_callsign(callsign):
     finalurl = '{}/{}/json'.format(callsign_url, callsign)
     callook_info = requests.get(finalurl)
     return callook_info.content
-
-
-def get_latlon(street_address):
-    """ Get the latitude and longitude of an address from google.
-        Not used, but I'm keeping it around for non us call signs"""
-    address_details = google(street_address)
-    print(address_details)
-    latlong = {'lat': address_details.lat, 'lon': address_details.lng}
-    return latlong
 
 
 def get_elevation(latlon):
@@ -46,12 +38,19 @@ class User(object):
         self.callsign = callsign
         self.lat = lat
         self.lon = lon
-        self.elevation = get_elevation({'lat': self.lat, 'lon': self.lon})
+        self.street_address = None
+        if self.lat is not None and self.lon is not None:
+            self.elevation = get_elevation({'lat': self.lat, 'lon': self.lon})
+        else:
+            self.elevation = None
         self.timezone = timezone
         self.new_user = False  # assume it's an existing
 
     def store_user(self):
         self.user_exist()
+        if self.lat is None or self.lon is None or self.elevation is None:
+            self.set_geo_stats()
+
         new_user = Location(callsign=self.callsign,
                             lat=self.lat,
                             lon=self.lon,
@@ -63,15 +62,28 @@ class User(object):
         """sets new_user to true if it's not in the Database."""
         try:
             Location.get(callsign=self.callsign)
-        except:
-            print('Location does not exist')
+        except Exception as e:
+            print('{} - Location does not exist'.format(e))
             self.new_user = True
+
+    def set_geo_stats(self):
+        address_details = google(self.street_address)
+        self.lat = address_details.lat
+        self.lon = address_details.lng
+        self.elevation = get_elevation({'lat': self.lat, 'lon': self.lon})
 
 
 if __name__ == '__main__':
-    test_callsign = config['default_location']['callsign']
+    # test_callsign = config['default_location']['callsign']
+    test_callsign = 'N7M'
     test_timezone = config['default_location']['timezone']
     fullLoc = json.loads(lookup_callsign(test_callsign).decode())
-    myUser = User(test_callsign, fullLoc['location']['latitude'], fullLoc['location']['longitude'], test_timezone)
-    myUser.store_user()
+    if fullLoc['status'] == 'VALID':
+        myUser = User(test_callsign, fullLoc['location']['latitude'], fullLoc['location']['longitude'], test_timezone)
+        myUser.store_user()
+    else:
+        myUser = User(test_callsign, None, None, test_timezone)
+        myUser.street_address = input('Callsign of {} was not found, please enter a street address: '.format(test_callsign))
+        myUser.store_user()
+
     print('Added {}'.format(myUser.callsign))
