1cc4259f64cb70067ec486382f42a6cb93daf38a
thurston <matt.thurston@gmail.com>
2016-04-29 22:46:32 -0600

    Add first pass for GET and PUT methods on User

--- src/UserManager.py
+++ src/UserManager.py
@@ -4,6 +4,7 @@ from geocoder import google
 import requests
 from model import *
 import sys
+import falcon
 
 # Globals
 try:
@@ -34,11 +35,11 @@ def get_elevation(latlon):
 
 
 class User(object):
-    def __init__(self, callsign, lat, lon, timezone):
+    def __init__(self, callsign, lat, lon, timezone, street_address = None):
         self.callsign = callsign
         self.lat = lat
         self.lon = lon
-        self.street_address = None
+        self.street_address = street_address
         if self.lat is not None and self.lon is not None:
             self.elevation = get_elevation({'lat': self.lat, 'lon': self.lon})
         else:
@@ -57,6 +58,7 @@ class User(object):
                             elevation=self.elevation,
                             timezone=self.timezone)
         new_user.save(force_insert=self.new_user)
+        print('User Stored')
 
     def user_exist(self):
         """sets new_user to true if it's not in the Database."""
@@ -68,9 +70,57 @@ class User(object):
 
     def set_geo_stats(self):
         address_details = google(self.street_address)
+        print(address_details)
         self.lat = address_details.lat
         self.lon = address_details.lng
         self.elevation = get_elevation({'lat': self.lat, 'lon': self.lon})
+
+class UserAPI(object):
+
+    def on_get(self, req, resp, callsign):
+        user = Location.get(callsign=callsign)
+        userdict = {
+            'callsign': user.callsign,
+            'lat': user.lat,
+            'lon': user.lon,
+            'elevation': user.elevation
+        }
+        resp.body = json.dumps(userdict)
+        resp.content_type = 'Application/JSON'
+        resp.status = falcon.HTTP_200
+
+    def on_put(self, req, resp, callsign):
+        if req.content_type == 'application/json':
+            api_input = req.stream.read().decode()
+            input_obj = json.loads(api_input)
+            if input_obj['lat'] is None and input_obj['long'] is None and input_obj['street_address'] is None:
+                # TODO: add lookup here
+                resp.status = falcon.HTTP_400
+                resp_dict = {'error': 'Invalid Parameters',
+                             'details': 'For Non US Callsigns, lat and long or street_address must be provided'}
+                resp.body = json.dumps(resp_dict)
+            else:
+                try:
+                    foo = User(input_obj['callsign'],
+                               input_obj['lat'],
+                               input_obj['long'],
+                               input_obj['timezone'],
+                               input_obj['street_address'])
+                    foo.store_user()
+                    resp.status = falcon.HTTP_204
+                except TypeError as e:
+                    resp.status = falcon.HTTP_500
+                    resp_dict = {'error': 'Invalid value for property provided',
+                                 'details': e.args}
+                    resp.body = json.dumps(resp_dict)
+                except KeyError as k:
+                    resp.status = falcon.HTTP_400 # This should probably be 422, but my falcon install doesn't have it.
+                    resp_dict = {'error': 'Missing Property',
+                                 'property name': k.args[0]}
+                    resp.body = json.dumps(resp_dict)
+        else:
+            resp.status = falcon.HTTP_415
+            resp.body = '{"error":"Content must be sent with a type of application/json"}'
 
 
 if __name__ == '__main__':
