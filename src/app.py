7e14b9584a09adc7d9c3e32f6c4998ec6beae57b
thurston <matt.thurston@gmail.com>
2016-05-01 22:30:44 -0600

    add timeslot route

--- src/app.py
+++ src/app.py
@@ -1,8 +1,12 @@
 import falcon
 import UserManager
+import TimeSlotHandler
 
 api = application = falcon.API()
 
 
 user = UserManager.UserAPI()
 api.add_route('/user/{callsign}', user)
+
+timeslots = TimeSlotHandler.TimeSlotAPI()
+api.add_route('/user/{callsign}/timeslots', timeslots)
