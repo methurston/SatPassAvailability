86df2bc949bfe69f8c51138a30d693ad8f87cee0
thurston <matt.thurston@gmail.com>
2016-05-06 22:38:42 -0600

    Add basic document

--- src/app.py
+++ src/app.py
@@ -9,9 +9,37 @@ TleHandler.UpdateTLE()
 
 user = UserManager.UserAPI()
 api.add_route('/user/{callsign}', user)
+# PUT  Add or Update a user.
+# Body:
+# {
+#     "lat": ,
+#     "long": ,
+#     "timezone": "America/Boise",
+#     "street_address": null
+# }
+# If using a US Callsign,  send lat, long, and street address as null.
+# Otherwise send lat, long, or street_adress
+#
+# GET  Returns stored data for a callsign.
 
 timeslots = TimeSlotHandler.TimeSlotAPI()
 api.add_route('/user/{callsign}/allslots', timeslots)
+# PUT  Add a new availabilty timeslot.
+# Body:
+# {
+#     "days": ,
+#     "start_time": ,
+#     "duration"
+# }
+# GET
+# Returns an expansion of the next week of time slots.
+
 
 slotwithpass = AvailablePasses.AvailablePassAPI()
 api.add_route('/user/{callsign}/availablepass', slotwithpass)
+# GET
+# Query Paramters:
+# sat_list = comma separated list of satellite names (no spaces).
+# min_elevation = Minimum elevation (in degrees) to consider a pass valid.  If this is omitted, any pass over 0 degrees
+# will be returned.
+# Returns any upcoming pass that conincides with the list returned by GETting allslots
